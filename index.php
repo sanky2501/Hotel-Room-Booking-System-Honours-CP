65b1a759c8316fea5ad2bcaff835f4df3805b018
RD328 <67415809+RD328@users.noreply.github.com>
2021-01-31 11:51:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <body id="page-top">
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Movie Theater Seat Reservation</a>
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">Now Showing</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto my-2 my-lg-0">
@@ -32,7 +32,7 @@
        ?>
         <!-- Footer-->
         <footer class="bg-light py-5">
-            <div class="container"><div class="small text-center text-muted">Copyright © 2020 - Movie Theater Seat Reservation System</div></div>
+            <div class="container"><div class="small text-center text-muted">Copyright © 2021 -Booking System</div></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
